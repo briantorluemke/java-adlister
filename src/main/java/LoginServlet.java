894b04611328cbad0395726d1540b437239d69c2
Brian Torluemke <briantorluemke@gmail.com>
2018-06-05 00:36:06 -0500

    added functionality to take user back to profile page if already logged in.

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,11 +3,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("loggedin") == "true") {
+            response.sendRedirect("/profile");
+            return;
+        }
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
@@ -17,8 +22,12 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username);
+            session.setAttribute("loggedin", "true");
             response.sendRedirect("/profile");
-        } else {
+        } else
+            {
             response.sendRedirect("/login");
         }
     }
